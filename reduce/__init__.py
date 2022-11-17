3b5b8f9f3cd3a976f93ae5b3433aad9d1ea9b646
zszprz <118436773+zszprz@users.noreply.github.com>
2022-11-17 12:20:49 +0100

    Initial commit

---
+++ reduce/__init__.py
@@ -0,0 +1,19 @@
+# type: ignore[attr-defined]
+"""Reduce is a python package that help with pairwise comparison matrices computations, such as CR reductions and calculate multiple indexes"""
+
+import sys
+
+if sys.version_info >= (3, 8):
+    from importlib import metadata as importlib_metadata
+else:
+    import importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
